0db825e9e799721abee57563431e7ff1729ce0c6
archana <ms.archanacusat2015@gmail.com>
2016-08-12 08:26:05 +0530

    network status is added

--- app/src/main/java/com/robosoft/archana/paytm/HomePageActivity.java
+++ app/src/main/java/com/robosoft/archana/paytm/HomePageActivity.java
@@ -2,6 +2,9 @@ package com.robosoft.archana.paytm;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.robosoft.archana.paytm.Adapter.HomePageAdapter;
 import com.robosoft.archana.paytm.Network.DownloadManager;
+import com.robosoft.archana.paytm.Network.NetworkStatus;
 import com.robosoft.archana.paytm.Network.VolleyHelper;
 import com.robosoft.archana.paytm.Util.HomePage;
 import com.robosoft.archana.paytm.Util.HomePageLayout;
@@ -22,7 +26,7 @@ import com.robosoft.archana.paytm.Util.ViewUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener{
 
     private String url = "https://dl.dropboxusercontent.com/s/u1rnvb8oow2xs5c/homepage.json?dl=0";
     private ImageView mImgErrorMsg;
@@ -31,6 +35,8 @@ public class HomePageActivity extends AppCompatActivity {
     private ProgressDialog mProgressDialog;
     private RecyclerView mHomePageRecycleView;
     private List<HomePageLayout> mHomePageLayout;
+    private CoordinatorLayout mCoordinatorLayout;
+    private FloatingActionButton mFloatBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,50 +49,59 @@ public class HomePageActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mProgressDialog = new ProgressDialog(this);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.container);
         mHomePageRecycleView = (RecyclerView) findViewById(R.id.layout_recycler);
         mImgErrorMsg = (ImageView) findViewById(R.id.networkerrorimg);
+        mFloatBtn = (FloatingActionButton) findViewById(R.id.fab);
+        mFloatBtn.setOnClickListener(this);
+
     }
     private void downloadServerData() {
-        ViewUtils.showProgressDialog(mProgressDialog);
-        DownloadManager.downloadData(this, url, new Response.Listener<HomePage>() {
-            @Override
-            public void onResponse(HomePage response) {
-                ViewUtils.dismissProgressDialog(mProgressDialog);
-                if (response != null) {
-                    mHomePageLayout = response.homePageLayout;
-                    for (int i = 0; i < mHomePageLayout.size(); i++) {
-                        List<Items> items = mHomePageLayout.get(i).items;
-                        if (mHomePageLayout.get(i).layout != null && mHomePageLayout.get(i).layout.equals("carousel-1")) {
-                            for (int j = 0; j < items.size(); j++) {
-                                mImgUrlList.add(items.get(j).imageUrl);
-                            }
-                        } else if (mHomePageLayout.get(i).layout != null && mHomePageLayout.get(i).layout.equals("row")) {
-                            for (int j = 0; j < items.size(); j++) {
-                                mRowLytList.add(items.get(j));
+        if(NetworkStatus.isNetworkAvailable(this)) {
+            ViewUtils.showProgressDialog(mProgressDialog);
+            DownloadManager.downloadData(this, url, new Response.Listener<HomePage>() {
+                @Override
+                public void onResponse(HomePage response) {
+                    ViewUtils.dismissProgressDialog(mProgressDialog);
+                    if (response != null) {
+                        mHomePageLayout = response.homePageLayout;
+                        for (int i = 0; i < mHomePageLayout.size(); i++) {
+                            List<Items> items = mHomePageLayout.get(i).items;
+                            if (mHomePageLayout.get(i).layout != null && mHomePageLayout.get(i).layout.equals("carousel-1")) {
+                                for (int j = 0; j < items.size(); j++) {
+                                    mImgUrlList.add(items.get(j).imageUrl);
+                                }
+                            } else if (mHomePageLayout.get(i).layout != null && mHomePageLayout.get(i).layout.equals("row")) {
+                                for (int j = 0; j < items.size(); j++) {
+                                    mRowLytList.add(items.get(j));
+                                }
                             }
                         }
                     }
+                    setHomepagerAdapter();
+
                 }
-                setHomepagerAdapter();
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    ViewUtils.dismissProgressDialog(mProgressDialog);
+                    mImgErrorMsg.setVisibility(View.VISIBLE);
+                }
+            });
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                ViewUtils.dismissProgressDialog(mProgressDialog);
-                mImgErrorMsg.setVisibility(View.VISIBLE);
-            }
-        });
-
-
+        }
+        else {
+            mFloatBtn.setVisibility(View.VISIBLE);
+            setSnackBar();
+        }
     }
 
 
     private void setHomepagerAdapter() {
+        mFloatBtn.setVisibility(View.GONE);
         HomePageAdapter homePageAdapter = new HomePageAdapter(mHomePageLayout, this, mImgUrlList, mRowLytList);
         mHomePageRecycleView.setLayoutManager(ViewUtils.setLinearLayoutManager(LinearLayoutManager.VERTICAL, this));
         mHomePageRecycleView.setAdapter(homePageAdapter);
-       // homePageAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -96,5 +111,24 @@ public class HomePageActivity extends AppCompatActivity {
         VolleyHelper.getInstance(this).cancelRequest(url);
 
     }
+    private void setSnackBar() {
+        Snackbar.make(mCoordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                .setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (NetworkStatus.isNetworkAvailable(getApplicationContext())) {
+                            downloadServerData();
 
+                        }
+                    }
+                }).show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.fab){
+            setSnackBar();
+        }
+    }
 }
